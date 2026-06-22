977bdd6738238d5240f271f44d22801304f5c778
sixue0805-wq <sixue@0805@gmail.com>
2026-06-22 17:38:05 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"==================欢迎来到机房预约系统=================="<<endl;
+    cout<<endl<<"请输入您的身份"<<endl;
+    cout<<"\t\t-------------------------\n"<<endl;
+    cout<<"\t\t                         \n"<<endl;
+    cout<<"\t\t         1. 学生          \n"<<endl;
+    cout<<"\t\t                          \n"<<endl;       
+    cout<<"\t\t         2. 老师          \n"<<endl;
+    cout<<"\t\t                          \n"<<endl;
+    cout<<"\t\t         3. 管理员         \n"<<endl;
+    cout<<"\t\t                          \n"<<endl;
+    cout<<"\t\t          0.退出           \n"<<endl;
+    cout<<"\t\t                          \n"<<endl;
+    cout<<"\t\t-------------------------- \n"<<endl;
+    cout<<"请输入您的选择：";
+    cout << "按回车退出...";
+    cin.get();  
+    return 0;
+}
